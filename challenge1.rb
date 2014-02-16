2311919babddfb19a9d47056b3984be7f14c008b
alessandro <alessandro.baffa@gmail.com>
2014-02-16 09:21:47 +0100

    Update after review.

--- challenge1.rb
+++ challenge1.rb
@@ -1,12 +1,12 @@
 def calculateAge(seconds)
-  util = (60.0 * 60.0 * 24 * 365)
-  year_raw = (seconds/util).round(2)
+  util_for_year_calculation = (60.0 * 60 * 24 * 365)
+  year_raw = (seconds/util_for_year_calculation).round(2)
   year_string = year_raw.to_s
   year_split = year_string.split('.')
   year = year_split.first
-  month = year_split.last.to_i.round
+  month = year_split.last.to_f.round
   puts "I'm #{year} years and #{month} months old."
-
+  [year, month]
 end
 
 calculateAge(979000000)
