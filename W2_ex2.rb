76a4ef915b44a2d1191194ee71538562069666ac
alessandro <alessandro.baffa@gmail.com>
2014-02-18 21:53:33 +0100

    update using block.

--- W2_ex2.rb
+++ W2_ex2.rb
@@ -1,3 +1,11 @@
-s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
-lines = s.split(/\n/)
-lines.each_with_index{|line, index| puts "Line #{index+1}: " + line}
+#s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+#lines = s.split(/\n/)
+#lines.each_with_index{|line, index| puts "Line #{index+1}: " + line}
+
+def block
+  s = "Welcome to the forum.\nHere you can learn Ruby.\nAlong with other members.\n"
+  lines = s.split(/\n/)
+  lines.each_with_index{|line, index| yield(line, index)}
+end
+
+block{|line, index| puts "Line #{index+1}: " + line}
