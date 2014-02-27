3f0e89ccce933eb727091509c9725683002d0717
alessandro <alessandro.baffa@gmail.com>
2014-02-27 22:51:16 +0100

    added the delete method in the end.

--- Week3/W3exe3.rb
+++ Week3/W3exe3.rb
@@ -1,8 +1,10 @@
+tmp_dir = "tmp"
 puts "Your current directory is : " + Dir.pwd
-new_dir = File.join(Dir.pwd, "tmp")
+new_dir = File.join(Dir.pwd, tmp_dir)
 puts "Creating new dir : " + new_dir
 Dir.open(new_dir)
 Dir.chdir(new_dir) do
   puts "Now you're in : " + Dir.pwd
 end
 puts "You're now back to: " + Dir.pwd
+Dir.delete(tmp_dir)
