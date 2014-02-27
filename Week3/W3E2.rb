0fcd5a6c38c41e39ba4fb166e0154a50e6fc82da
alessandro <alessandro.baffa@gmail.com>
2014-02-27 23:58:15 +0100

    Added method prompt and removed for statement.

--- Week3/W3E2.rb
+++ Week3/W3E2.rb
@@ -1,16 +1,18 @@
-puts "Please, insert the word you want to change: "
-word_to_change = gets.chomp
-puts "Now insert the word to use as replacement: "
-word_replace = gets.chomp
-puts "I'm going to replace '" + word_to_change + "' with the word '" + word_replace + "'"
-puts "Now insert the file name where you want to perform the replacement: "
-file_name = gets.chomp
+def prompt message
+  print message
+  gets.chomp
+end
+
+word_to_change = prompt "Please, insert the word you want to change: "
+word_replace = prompt "Now insert the word to use as replacement: "
+puts "I'm going to replace the word '#{word_to_change}' with the word '#{ word_replace }'."
+file_name = prompt "Now insert the file name where you want to perform this replacement: "
 
 lines = IO.readlines(file_name)
-  for line in lines
+  lines.each {|line|
     if line.include?(word_to_change)
       puts line.gsub! word_to_change, word_replace
     else
       puts line
     end
-  end
+  }
