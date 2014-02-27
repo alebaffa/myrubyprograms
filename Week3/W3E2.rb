bbeca418ef21957f719652c65f79ca9fed10e496
alessandro <alessandro.baffa@gmail.com>
2014-02-27 21:56:26 +0100

    updated using more clever stuff.

--- Week3/W3E2.rb
+++ Week3/W3E2.rb
@@ -1,9 +1,16 @@
-File.open('file.txt', 'r') do |f1|  
-  while line = f1.gets  
-    if(line.match('word'))
-      puts line.gsub! 'word', 'inserted word' 
+puts "Please, insert the word you want to change: "
+word_to_change = gets.chomp
+puts "Now insert the word to use as replacement: "
+word_replace = gets.chomp
+puts "I'm going to replace '" + word_to_change + "' with the word '" + word_replace + "'"
+puts "Now insert the file name where you want to perform the replacement: "
+file_name = gets.chomp
+
+lines = IO.readlines(file_name)
+  for line in lines
+    if line.include?(word_to_change)
+      puts line.gsub! word_to_change, word_replace
     else
       puts line
     end
-  end  
-end
+  end
