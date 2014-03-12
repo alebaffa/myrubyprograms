d03c3c20caaccf6684ca0b5f3eaca59faab883dc
alessandro <alessandro.baffa@gmail.com>
2014-03-13 00:55:04 +0100

    Fixed code style after Rubocop review.

--- Week5/gameboard.rb
+++ Week5/gameboard.rb
@@ -1,17 +1,18 @@
+# GameBoard will match the user input and rate his performance.
 class GameBoard
   def initialize
-    @range = [0,1,2,3,4,5,6]
+    @range = [0, 1, 2, 3, 4, 5, 6]
     @no_of_hits = 0
   end
 
   def set_locations_cells(locations)
     @numbers_to_guess = []
-    locations.each{|index| @numbers_to_guess.push(@range[index])}
+    locations.each { |index| @numbers_to_guess.push(@range[index]) }
   end
 
   def check_yourself(user_guess)
     if @numbers_to_guess.include?(user_guess.to_i)
-      @no_of_hits +=1
+      @no_of_hits += 1
       puts 'Hit'
       kill?
     else
