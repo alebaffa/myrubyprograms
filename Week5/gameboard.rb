ef21fc26921f6593176b87b1ca3958c9006a4749
alessandro <alessandro.baffa@gmail.com>
2014-03-12 22:29:38 +0100

    first commit.

---
+++ Week5/gameboard.rb
@@ -0,0 +1,28 @@
+class GameBoard
+  def initialize
+    @range = [0,1,2,3,4,5,6]
+    @no_of_hits = 0
+  end
+
+  def set_locations_cells(locations)
+    @numbers_to_guess = []
+    locations.each{|index| @numbers_to_guess.push(@range[index])}
+  end
+
+  def check_yourself(user_guess)
+    if @numbers_to_guess.include?(user_guess.to_i)
+      @no_of_hits +=1
+      puts 'Hit'
+      kill?
+    else
+      puts 'Miss'
+    end
+  end
+
+  def kill?
+    if @no_of_hits == 3
+      puts 'End'
+      'kill'
+    end
+  end
+end
